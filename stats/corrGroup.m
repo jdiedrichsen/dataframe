cf360de96997e6b99980bd38c1887449cf72e5ff
Atsushi Yokoi <at.yokoi.work@gmail.com>
2015-12-16 11:23:23 -0500

    update corrGroup: 	- made documentation clearer (i hope) 	- add option to use 'corrN'

--- stats/corrGroup.m
+++ stats/corrGroup.m
@@ -1,13 +1,17 @@
 function [Corr_within,Corr_between,Indicator] = corrGroup(In1,In2,varargin)
-%% [Corr_within,Corr_between] = corrGroup(In1,In2,varargin)
+%% function [Corr_within,Corr_between] = corrGroup(In1,In2,varargin)
 % calculates within- and between-group correlation given data matrices of two different groups.
 % 
 % Inputs:
-%     In1,In2: data matrices. row is supposed to each observation, and column is supposed to data points.
+%     In1: data (Na by M matrix).
+%     In2: data (Nb by M matrix). column size should be equal between In1 and In2.
+%     
+% corrGroup first calculates (Na+Nb) by (Na+Nb) pairwise correlation matrix, then split it into
+%  within- and between-group parts.
 %         
 % Options:
-%     type: correlation function ('Pearson', 'Spearman', 'Kendall'). Default is 'Pearson'
-%         
+%     type: correlation function ('Pearson', 'Spearman', 'Kendall', 'corrN'). Default is 'Pearson'
+%           'corrN' calculates correlation withoug intercept (normalised cross product).
 % Outputs:
 %     Corr_within: vector of within-group correlation. each element is pairwise correlation excluding the same observations
 %     Corr_between: vector of between-group correlation. each element is pairwise correlation excluding the same observations
@@ -18,6 +22,8 @@ function [Corr_within,Corr_between,Indicator] = corrGroup(In1,In2,varargin)
 % 
 %   [Corr_within,Corr_between,Indicator] = corrGroup(In1,In2,'type','Pearson');
 %   barplot(Indicator,[Corr_whithin;Corr_between]);
+% 
+% see also: corrN.m
 % 
 % ayokoi (2015/Dec)
 
@@ -51,8 +57,13 @@ Idx_within(1:Na,(Na+1):end) = false;
 Idx_between = false(Na+Nb);
 Idx_between(1:Na,(Na+1):end) = true;
 
-% Get overall pairwise correlation matrix using built-in corr function
-C = corr([In1;In2]','type',type);
+% Get overall pairwise correlation matrix using built-in corr function or corrN
+switch (type)
+    case 'corrN'
+        C = corrN([In1;In2]');
+    otherwise
+        C = corr([In1;In2]','type',type);
+end
 
 % Split pairwise correlations
 Corr_within = C(Idx_within);
